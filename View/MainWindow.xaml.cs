8ff63d30f498b8213a288123bfab50776e45c084
Captain Newt <42378068+NNeerly@users.noreply.github.com>
2021-01-13 06:32:27 +0800

    Custom Window Design, Button Style and more XAML Stuff

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using Ark.ViewModel;
+using System;
 using System.Windows;
+using System.Windows.Media;
 
 namespace Ark
 {
@@ -8,10 +10,60 @@ namespace Ark
 
         public MainWindow()
         {
-            InitializeComponent();
             DataContext = new MainWindowViewModel();
+            InitializeComponent();
             MainMenu.SelectedItem = 0;
         }
 
+        //Hide Window
+        private void hideClick(object sender, RoutedEventArgs e)
+        {
+
+            Window window = GetParentWindow((DependencyObject)sender);
+            window.WindowState = WindowState.Minimized;
+        }
+
+        //Resize Window
+        private void resizeClick(object sender, RoutedEventArgs e)
+        {
+            Window window = GetParentWindow((DependencyObject)sender);
+            if (window.WindowState == WindowState.Normal)
+            {
+                window.WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                window.WindowState = WindowState.Normal;
+            }
+        }
+
+        //Close Window
+        private void closeClick(object sender, RoutedEventArgs e)
+        {
+            Window window = GetParentWindow((DependencyObject)sender);
+            window.Close();
+        }
+
+        private Window GetParentWindow(DependencyObject child)
+        {
+
+            DependencyObject parentObject = VisualTreeHelper.GetParent(child);
+
+            if (parentObject == null)
+            {
+                return null;
+            }
+
+            Window parent = parentObject as Window;
+            if (parent != null)
+            {
+                return parent;
+            }
+            else
+            {
+                return GetParentWindow(parentObject);
+            }
+
+        }
     }
 }
